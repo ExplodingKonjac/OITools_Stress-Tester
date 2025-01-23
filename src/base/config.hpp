a47fc7e544552d0bea2a9662182437cc8d47b894
ExplodingKonjac <wangcj8@foxmail.com>
2025-01-23 15:18:13 +0800

    update version info

--- src/base/config.hpp
+++ src/base/config.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #define BOOST_PROCESS_VERSION 2
-#define OIT_STRESS_VERSION "2.0.0"
+#define OIT_STRESS_VERSION "2.0.1"
 #define COPYRIGHT "Copyright (C) 2024 ExplodingKonjac"
 
 #ifdef _WIN32
